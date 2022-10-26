f2829216b25c44ea7e1a3f6e84cd89298758e53f
SamueleMuraro <116717610+SamueleMuraro@users.noreply.github.com>
2022-10-26 12:21:36 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        String a = "e";
+        String b = "h";
+        String c = "o";
+        String d = "l";
+        String e = "r";
+        String f = "d";
+        String h = "w";
+        
+        System.out.println(b+a+d+d+c+ " "+h+c+e+d+f);
+    }
+}
